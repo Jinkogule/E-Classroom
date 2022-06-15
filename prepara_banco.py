ba0c9dee6c19dc31dc31b0b81c771f630a9bb386
Daniel Fernandes Pereira <daniel.pereira@gestqual.com.br>
2022-06-15 10:55:57 -0300

    ok

--- prepara_banco.py
+++ prepara_banco.py
@@ -25,22 +25,56 @@ cursor.execute("USE `projsoft`;")
 
 # criando tabelas
 TABLES = {}
+
+TABLES['Usuarios'] = ('''
+      CREATE TABLE `usuarios` (
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `nome` varchar(20) NOT NULL,
+      `idade` int(150) NOT NULL,
+      `email` varchar(256) NOT NULL,
+      `senha` varchar(100) NOT NULL,
+      `tipoUsuario` int(5) NOT NULL,
+      PRIMARY KEY (`id`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;''')
+
 TABLES['Produtos'] = ('''
       CREATE TABLE `produtos` (
       `id` int(11) NOT NULL AUTO_INCREMENT,
       `nome` varchar(50) NOT NULL,
-      `categoria` varchar(40) NOT NULL,
-      `valor` varchar(20) NOT NULL,
+      `descricao` varchar(100) NOT NULL,
+      `valor` int(5000) NOT NULL,
+      `usuarioId` int(11) NOT NULL,
+      `avaliacao` int(11) NOT NULL,
       PRIMARY KEY (`id`)
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;''')
 
-TABLES['Usuarios'] = ('''
-      CREATE TABLE `usuarios` (
-      `nome` varchar(20) NOT NULL,
-      `nickname` varchar(8) NOT NULL,
-      `senha` varchar(100) NOT NULL,
-      PRIMARY KEY (`nickname`)
+TABLES['Pedido'] = ('''
+      CREATE TABLE `produtos` (
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `total` int(5000) NOT NULL,
+      `status` varchar(50) NOT NULL,
+      `usuarioId` int(11) NOT NULL,
+      PRIMARY KEY (`id`)
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;''')
+
+TABLES['Pagamento'] = ('''
+      CREATE TABLE `produtos` (
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `formaDePagamento` int(2) NOT NULL,
+      `status` varchar(50) NOT NULL,
+      PRIMARY KEY (`id`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;''')
+
+TABLES['Carrinho'] = ('''
+      CREATE TABLE `produtos` (
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `quantidade` int(100) NOT NULL,
+      `subTotal` float(5000.0) NOT NULL,
+      `status` varchar(50) NOT NULL,
+      `usuarioId` int(11) NOT NULL,
+      PRIMARY KEY (`id`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;''')
+
 
 for tabela_nome in TABLES:
       tabela_sql = TABLES[tabela_nome]
