68b0e0ffce032609e561064ca84b4db4aab91489
Luisa <luisa.dirce.silva@gmail.com>
2022-07-13 20:27:37 -0300

    padrão de projeto singleton

---
+++ Business/Usuario/Administrador.py
@@ -0,0 +1,5 @@
+# Luisa - padrão de projeto Singleton matricula 217083095
+from UsuarioInterface import *
+class Administrador(UsuarioInterface):
+
+    def FixarComentario():
---
+++ Interface/UsuarioInterface.py
@@ -0,0 +1,20 @@
+# Luisa
+class UsuarioInterface:
+
+    def init(self, nome, endereco, cidade, estado, cpf, email):
+        self.nome = nome
+        self.endereco = endereco
+        self.cidade = cidade
+        self.estado = estado
+        self.cpf = cpf
+        self.email = email
+
+    def FazerPedido():
+    
+    def CancelarPedido():
+
+    def EditarPerfil():
+
+    def AvaliarProduto():
+
+    def ComentarNoProduto():
