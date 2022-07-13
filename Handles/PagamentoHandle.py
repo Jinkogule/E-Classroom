5c0bf0c57a5a104e1ff0179a73f3639eaa96c287
Daniel Fernandes Pereira <daniel.pereira@gestqual.com.br>
2022-07-13 16:44:59 -0300

    criação de classes

---
+++ Handles/PagamentoHandle.py
@@ -0,0 +1,8 @@
+class PagamentoHandle(db.Model):
+    #id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    #nome = db.Column(db.String(50), nullable=False)
+    #categoria = db.Column(db.String(40), nullable=False)
+    #valor = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
